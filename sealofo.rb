8691265ca238969aabbd49ba1295930860dd657c
夜神月 <31050499+First-Kira@users.noreply.github.com>
2025-07-31 16:10:18 -0400

    Add files via upload

---
+++ sealofo.rb
@@ -0,0 +1,65 @@
+Basic Applications
+
+1.  Safari: `open -a Safari`
+2.  TextEdit: `open -a TextEdit`
+3.  Contacts: `open -a Contacts`
+4.  Calendar: `open -a Calendar`
+5.  Reminders: `open -a Reminders`
+6.  Notes: `open -a Notes`
+7.  Maps: `open -a Maps`
+8.  Photos: `open -a Photos`
+9.  Messages: `open -a Messages`
+10. FaceTime: `open -a FaceTime`
+11. Mail: `open -a Mail`
+12. App Store: `open -a "App Store"`
+13. System Preferences: `open -a "System Preferences"`
+14. Activity Monitor: `open -a "Activity Monitor"`
+15. Console: `open -a Console`
+16. Disk Utility: `open -a "Disk Utility"`
+17. Keychain Access: `open -a "Keychain Access"`
+18. Terminal: `open -a Terminal`
+19. Automator: `open -a Automator`
+20. Script Editor: `open -a "Script Editor"`
+21. Dictionary: `open -a Dictionary`
+22. Calculator: `open -a Calculator`
+23. Chess: `open -a Chess`
+24. DVD Player: `open -a "DVD Player"`
+25. Photo Booth: `open -a "Photo Booth"`
+26. QuickTime Player: `open -a "QuickTime Player"`
+
+Web Browsers
+
+27. Google Chrome: `open -a "Google Chrome"`
+28. Firefox: `open -a Firefox`
+29. Opera: `open -a Opera`
+30. Brave Browser: `open -a Brave\ Browser`
+31. Microsoft Edge: `open -a "Microsoft Edge"`
+
+Productivity and Development
+
+32. Microsoft Word: `open -a "Microsoft Word"`
+33. Microsoft Excel: `open -a "Microsoft Excel"`
+34. Microsoft PowerPoint: `open -a "Microsoft PowerPoint"`
+35. Numbers: `open -a Numbers`
+36. Pages: `open -a Pages`
+37. Keynote: `open -a Keynote`
+38. Xcode: `open -a Xcode`
+39. Visual Studio Code: `open -a "Visual Studio Code"`
+40. Sublime Text: `open -a "Sublime Text"`
+41. Atom: `open -a Atom`
+42. BBEdit: `open -a BBEdit`
+
+Utilities and Other
+
+43. Adobe Photoshop: `open -a "Adobe Photoshop"`
+44. Adobe Illustrator: `open -a "Adobe Illustrator"`
+45. Adobe Acrobat: `open -a "Adobe Acrobat"`
+46. VLC: `open -a VLC`
+47. Spotify: `open -a Spotify`
+48. Discord: `open -a Discord`
+49. Slack: `open -a Slack`
+50. Zoom: `open -a zoom.us`
+51. Skype: `open -a Skype`
+52. iMovie: `open -a iMovie`
+53. GarageBand: `open -a GarageBand`
+54. Logic Pro X: `open -a "Logic Pro X"`
